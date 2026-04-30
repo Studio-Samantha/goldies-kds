f396c9be448e510505253e051ba21c61892da15f
Samantha Carey <fullsailuniversity-samanthacarey@MacBookPro.home.local>
2026-04-30 00:11:24 -0500

    Add fallback and simplify app boot

--- my-menu-app/src/main.jsx
+++ my-menu-app/src/main.jsx
@@ -3,23 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.getRegistrations().then((registrations) => {
-      registrations.forEach((registration) => registration.unregister());
-    }).catch(() => {
-      // Ignore service worker cleanup failures.
-    });
-    if ("caches" in window) {
-      caches.keys().then((keys) => {
-        keys.forEach((key) => caches.delete(key));
-      }).catch(() => {
-        // Ignore cache cleanup failures.
-      });
-    }
-  });
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
